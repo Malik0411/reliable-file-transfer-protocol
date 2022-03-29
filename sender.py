28da02fef626c023f1570e672ea75bd586561557
Malik0411 <43456911+Malik0411@users.noreply.github.com>
2022-03-29 18:04:08 -0400

    Finished sender

--- sender.py
+++ sender.py
@@ -1,4 +1,6 @@
 import sys
+import json
+from multiprocessing import Process
 from socket import *
 from helper import *
 
@@ -19,7 +21,7 @@ class Server:
         except ValueError as e:
             printexit(f"ERROR: <receiver_port>={sys.argv[2]}, <sender_port>={sys.argv[3]} and <timeout>={sys.argv[4]} must be integers :: {e}")
         
-        self.seqnum = 0
+        self.acked = dict()
         self.receiver = (self.receiver_address, self.receiver_port)
     
     def Setup_UDP_Socket(self):
@@ -31,21 +33,85 @@ class Server:
             printexit(f"ERROR: failed to setup the UDP socket :: {e}")
 
     def Send_Packet(self):
+        # Initializes the seqnum
+        seqnum = 0
+
         # Reads data from <filename> and sends up to 500 characters
         # worth of data until reaching EOF
         f = open(self.filename)
         data = f.read(500)
         while data != "":
-            packet = f"{1}\n{self.seqnum}\n{len(data)}\n{data}"
-            self.sock.sendto(packet.encode(), self.receiver)
-            self.seqnum += len(data)
+            # Creating new entry for this seqnum
+            if seqnum not in self.acked:
+                self.acked[seqnum] = False
+
+            # Checking if this packet needs to be sent
+            if self.acked[seqnum] == False:
+                packet = {
+                    'type': 1,
+                    'seqnum': seqnum,
+                    'length': len(data),
+                    'data': data
+                }
+                s = json.dumps(packet) # serialized json from dict
+                self.sock.sendto(s.encode(), self.receiver)
+
+            seqnum += len(data)
             data = f.read(500)
         f.close()
+    
+    def Wait_Timeout(self):
+        # Wait until all packets are received and acknowledged
+        while all(value == True for value in self.acked.values()):
+            message, _ = self.sock.recvfrom(2048)
+            ds = json.loads(message.decode()) # deserialized dict from json
+            if ds['type'] == 0 and self.acked[ds['seqnum']] == False:
+                self.acked[ds['seqnum']] = True
+    
+    def Send_EOT(self):
+        # Creating the EOT packet
+        packet = {
+            'type': 2,
+            'seqnum': -1,
+            'length': 0,
+            'data': ""
+        }
+
+        # Sending the EOT packet
+        s = json.dumps(packet) # serialized json from dict
+        self.sock.sendto(s.encode(), self.receiver)
+
+        # Receiving the EOT packet
+        message, _ = self.sock.recvfrom(2048)
+        ds = json.loads(message.decode()) # deserialized dict from json
+        assert ds['type'] == 2 and ds['length'] == 0
 
 
 def main():
     sender = Server()
     sender.Setup_UDP_Socket()
+    while True:
+        # Send the unacked packets
+        sender.Send_Packet()
+
+        # Creating a thread to receive acks until timeout
+        p = Process(target=sender.Wait_Timeout)
+        p.start()
+
+        # Wait for timeout or until the process finishes
+        p.join(int(round(sender.timeout / 1000)))
+
+        # Check if the thread is still active
+        if p.is_alive():
+            print("Timeout occurred... Resending packets")
+            p.terminate()
+            p.join()
+            continue
+        break
+
+    # Sends the EOT signal
+    # Waits for the receiver until exiting
+    sender.Send_EOT()
 
 if __name__ == "__main__":
     main()
